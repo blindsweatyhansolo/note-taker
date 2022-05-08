5e6f0a5524e8eb7b2e0e3e24953e9e8717b3df35
J Semidey <mtsemidey@gmail.com>
2022-05-08 19:18:34 -0400

    missing __dirname from most of the write/readfile functions causing errors, fixed and working in insomnia

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -35,8 +35,8 @@ router.post('/notes', (req, res) => {
     
     // new note object, uses uniqid() to create random id
     let newNote = {
-        title: note.title,
-        text: note.text,
+        title: req.body.title,
+        text: req.body.text,
         id: uniqid()
     };
     
@@ -45,76 +45,51 @@ router.post('/notes', (req, res) => {
 
     // stringify notes in array, set as variable
     const stringNote = JSON.stringify(notes);
-    // res.json(notes);
+    res.json(notes);
 
     // add stringified note to db.json using writeFile
-    fs.writeFile( '../../db/db.json', stringNote, (err) => {
+    fs.writeFile(path.join(__dirname, '../../db/db.json'), stringNote, (err) => {
         if (err) {
             console.log(err);
         } else {
             // confirmation log
             console.log(`New note successfully saved.`);
-            return res.json(notes);
+            // return res.json(notes);
         }
     });
 });
 
 // // DELETE request to remove note with id
-// router.delete('/notes/:id', (req, res) => {
-//     // confirmation log
-//     console.log(`${req.method} request received to delete note.`);
-
-//     // grab id from request, save as new variable
-//     let noteId = req.params.id;
-
-//     // read the current db.json file to compare grabbed id against saved notes ids, then re-write file without deleted note
-//     fs.readFile(path.join('../../db/db.json'), 'utf-8', (err, data) => {
-//         // save parsed data from db.json as variable
-//         let updatedNotes = JSON.parse(data).filter((note) => {
-//             // return note ids that don't match noteId from request
-//             return note.id !== noteId;
-//         });
-//         notes = updatedNotes;
-
-//         // stringify notes in updatedNotes array, set as variable
-//         const stringNote = JSON.stringify(updatedNotes);
-
-//         // add updated stringified notes to db.json using writeFile
-//         fs.writeFile(path.join(__dirname, '../../db/db.json'), stringNote, (err) => {
-//             if (err) console.log(err);
-//             else {
-//                 // confirmation log
-//                 console.log(`Note successfully deleted.`)
-//             }
-//         });
-//         res.json(stringNote);
-//     });
-// });
-
 router.delete('/notes/:id', (req, res) => {
     // confirmation log
     console.log(`${req.method} request received to delete note.`);
 
     // grab id from request, save as new variable
     let noteID = req.params.id;
+    // console.log(noteID);
 
     // read the current db.json file to compare grabbed id against saved notes ids, then re-write file without deleted note
-    fs.readFile("../../db/db.json", "utf8", function (err, data) {
-      let updatedNotes = JSON.parse(data).filter((note) => {
-        return note.id !== noteID;
-      });
+    fs.readFile(path.join("db/db.json"), "utf8", (err, data) => {
+        // save parsed data from db.json as variable
+        let updatedNotes = JSON.parse(data).filter((note) => {
+            // return note ids that don't match noteID from request
+            return note.id !== noteID;
+        });
 
-      notes = updatedNotes;
+        notes = updatedNotes;
 
-      const stringifyNote = JSON.stringify(updatedNotes);
+        // stringify notes in updatedNotes array, set as variable
+        const stringifyNote = JSON.stringify(updatedNotes);
 
-      fs.writeFile('../../db/db.json', stringifyNote, (err) => {
+        // add updated stringified notes to db.json using writeFile
+        fs.writeFile(path.join(__dirname, '../../db/db.json'), stringifyNote, (err) => {
         if (err) console.log(err);
         else {
-          console.log("Note successfully deleted from db.json");
+            console.log("Note successfully deleted from db.json");
         }
-      });
-      res.json(stringifyNote);
+        });
+
+        res.json(updatedNotes);
     });
   });
 
